eaf42840efe3bb907e9be9cd17cc0fa969318030
alexdev34 <alexandresch01@hotmail.com>
2024-01-09 12:47:33 +0100

    modal

--- credentialsmanager/views.py
+++ credentialsmanager/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template import loader
 from .models import Site
+from .forms import SiteForm
 
 # Create your views here.
 
@@ -18,3 +19,18 @@ def supprimer_enregistrement(request, pk):
     objet_a_supprimer = get_object_or_404(Site, pk=pk)
     objet_a_supprimer.delete()
     return redirect('index')
+
+
+def liste_sites(request):
+    sites = Site.objects.all()
+    return render(request, 'liste_sites.html', {'sites': sites})
+
+def ajout_site(request):
+    if request.method == 'POST':
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('liste_sites')
+    else:
+        form = SiteForm()
+    return render(request, 'ajout_site.html', {'form': form})
