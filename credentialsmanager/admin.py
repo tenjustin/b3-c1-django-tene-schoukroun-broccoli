578f25228d9d1adc7af02cd0c698267e7feda793
alexdev34 <alexandresch01@hotmail.com>
2024-01-09 12:47:33 +0100

    modal
    CSV 12-01-2024
    
    Delete button
    
    db maj
    
    menage
    
    menage 2

--- credentialsmanager/admin.py
+++ credentialsmanager/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 from credentialsmanager.models import Site
-
-# Register your models here.
+from .models import Site
 
 admin.site.register(Site)
--- credentialsmanager/views.py
+++ credentialsmanager/views.py
@@ -1,20 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import render
 from django.template import loader
-from .models import Site
+from credentialsmanager.models import Site
+from credentialsmanager.scripts import delete
 
 # Create your views here.
 
 def index(request):
     password_list = []
-    password_list.append(Site(1, "example", "example.com", "Justin", "Password123"))
+    password_list.append(Site("example", "example.com", "Justin", "Password123"))
     template = loader.get_template("index.html")
     context = {
         "password_list": password_list,
     }
     return HttpResponse(template.render(context, request))
 
-def supprimer_enregistrement(request, pk):
-    objet_a_supprimer = get_object_or_404(Site, pk=pk)
-    objet_a_supprimer.delete()
-    return redirect('index')
+def delete_action(request):
+    if request.method == 'POST':
+        delete.run_delete()  # Supposons que run_delete() est une fonction dans delete.py
+        # Rediriger ou afficher un message de succès
+        return render(request, 'index.html')
+    else:
+        # Gérer les autres méthodes (GET, etc.)
+        return render(request, 'error.html')
