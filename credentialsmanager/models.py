0aa72d40b52d9a9063c7b43a7fe759128094a477
Justin <tene.justin@gmail.com>
2024-01-09 12:55:55 +0100

    Ajout de la page de liste

--- credentialsmanager/models.py
+++ credentialsmanager/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 
 class Site(models.Model):
@@ -5,6 +6,12 @@ class Site(models.Model):
     url = models.URLField()
     identifiant = models.CharField
     mot_de_passe = models.CharField
+    
+    def __init__(self, nom, url, identifiant, mot_de_passe):
+        self.nom = nom
+        self.url = url
+        self.identifiant = identifiant
+        self.mot_de_passe = mot_de_passe
 
     def __str__(self):
         return self.nom
--- credentialsmanager/views.py
+++ credentialsmanager/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Site
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World !")
+    password_list = []
+    password_list.append(Site("example", "example.com", "Justin", "Password123"))
+    template = loader.get_template("index.html")
+    context = {
+        "password_list": password_list,
+    }
+    return HttpResponse(template.render(context, request))
