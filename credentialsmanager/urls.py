8adcbda19e0ee3ba5920aa8a203c367a8b666c5c
Justin <tene.justin@gmail.com>
2024-01-31 15:27:36 +0100

    ajout de l'import/export au format csv

--- credentialsmanager/urls.py
+++ credentialsmanager/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.liste_sites, name="liste_sites"),
     path("supprimer/<int:pk>/", views.supprimer_enregistrement, name='supprimer_enregistrement'),
-    path("ajout_site/", views.ajout_site_action, name="ajout_site"),
     path("modification_site/<int:pk>/", views.modifier_site, name="modification_site"),
+    path("ajout_site", views.ajout_site_action, name="ajout_site"),
+    path('export-csv/', views.exporter_identifiants_csv, name='export_csv'),
+    path('import-csv/', views.import_csv, name='import_csv'),
+    path('page-import/', views.page_import, name='page_import')
 ]
