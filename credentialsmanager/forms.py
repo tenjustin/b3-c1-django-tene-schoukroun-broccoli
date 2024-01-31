1f6aa6d5db0f593c765b30e8ff560fbbc370ade6
alexdev34 <alexandresch01@hotmail.com>
2024-01-31 16:14:38 +0100

    finition de la page de modification avec boostrap

--- credentialsmanager/forms.py
+++ credentialsmanager/forms.py
@@ -6,3 +6,9 @@ class SiteForm(forms.ModelForm):
     class Meta:
         model = Site  
         fields = ['nom', 'url', 'identifiant', 'mot_de_passe']  
+        widgets = {
+            'nom': forms.TextInput(attrs={'class': 'form-control'}),
+            'url': forms.URLInput(attrs={'class': 'form-control'}),
+            'identifiant': forms.TextInput(attrs={'class': 'form-control'}),
+            'mot_de_passe': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
